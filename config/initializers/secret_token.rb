5bcefe4d172d671273fc0dba4d3d77ece9c47975
Bryantb06 <btornebarraza@gmail.com>
2013-02-07 11:54:40 -0500

    primeros cambios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recetario::Application.config.secret_token = '0a2da8f62bffed5dd03eed0291cc09260aca69aadd8144f35fea35cda33f98bc08ba7093165ada5bb1a2445d3ef5ab2a446a25197ab3d785b4bcacfc5ae7601c'
